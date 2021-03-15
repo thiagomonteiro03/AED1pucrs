59b73971e3ae426a10103ac508c1092048d7dd3f
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-03-15 00:16:04 -0300

    proposta da aula entregue

--- aula2.java
+++ aula2.java
@@ -1,12 +1,35 @@
 import java.util.Random;
 
+import javax.swing.plaf.TreeUI;
+
 public class aula2 {
 
 	public static void main(String args[]){
 
 		int [] vet = geraVetor(5, 5);
 		int [] res = new int[vet.length];
+		int [] res1 = new int[vet.length];
 		int cont = 0;
+		int cont1 = 0;
+		int cont2 = vet.length -1;
+		int contRev = vet.length - 1;
+		Boolean stats = true;
+
+		for(int i = 0; i<vet.length;i++){
+			if(vet[i]%2==0){
+				res1[cont1] = vet[i];
+				cont1++;
+			}else{
+				stats = true;
+				for(int j = 0; stats;contRev--){
+					if(vet[contRev]%2>0){
+						res1[cont2] = vet[contRev];
+						stats = false;
+						cont2--;
+					}
+				}
+			} 
+		}
 
 		for(int i = 0; i<vet.length;i++){
 			if(vet[i]%2==0){
@@ -22,10 +45,17 @@ public class aula2 {
 			} 
 		}
 
+		System.out.println("Números originais: ");
 		for(int i = 0; i<vet.length;i++){
 			System.out.print(vet[i]+", ");
 		}
 		System.out.println("");
+		System.out.println("Leitura única: ");
+		for(int i = 0; i<res1.length;i++){
+			System.out.print(res1[i]+", ");
+		}
+		System.out.println("");
+		System.out.println("Leitura repetitiva: ");
 		for(int i = 0; i<res.length;i++){
 			System.out.print(res[i]+", ");
 		}
